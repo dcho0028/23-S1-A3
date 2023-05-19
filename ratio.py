179c5178c7d6f5374b9d8e77a963d19a5ab320a1
dcho0028 <dcho0028@student.monash.edu>
2023-05-19 14:06:56 +0800

    doing task 3 halfway

--- ratio.py
+++ ratio.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+
+import math
 from typing import Generic, TypeVar
 from math import ceil
 from bst import BinarySearchTree
@@ -13,7 +15,7 @@ class Percentiles(Generic[T]):
     
     def add_point(self, item: T):
         self.points.append(item)
-        self.points.sort()
+        #self.points.sort()
     
     def remove_point(self, item: T):
         if item in self.points:
@@ -21,9 +23,26 @@ class Percentiles(Generic[T]):
 
     def ratio(self, x, y):
         n = len(self.points)
-        x_count = max(1, int(x * n / 100))
-        y_count = max(1, int(y * n / 100))
-        return self.points[:x_count] + self.points[-y_count:]
+        x_count = max(0, math.ceil(x * n / 100))
+        y_count = max(1, math.ceil(y * n / 100))
+        sorted_points = self.quicksort(self.points)
+        return sorted_points[x_count:-y_count]
+
+
+
+    #since sort and sorted are not allowed i made a simple quicksort
+    # however base on this ed discussion https://edstem.org/au/courses/10179/discussion/1376885
+    # it seems that i can make my own sort and then implement it so i decided to use quickort
+    def quicksort(self,arr:[]):
+        if len(arr) <= 1:
+            return arr
+
+        pivot = arr[len(arr) // 2]
+        left = [x for x in arr if x < pivot]
+        middle = [x for x in arr if x == pivot]
+        right = [x for x in arr if x > pivot]
+
+        return self.quicksort(left) + middle + self.quicksort(right)
 
 if __name__ == "__main__":
     points = list(range(50))
--- threedeebeetree.py
+++ threedeebeetree.py
@@ -13,7 +13,19 @@ class BeeNode:
     subtree_size: int = 1
 
     def get_child_for_key(self, point: Point) -> BeeNode | None:
-        raise NotImplementedError()
+        x,y,z = self.key
+        bx,by,bz = point
+
+        octant = 0
+        if bx >= x:
+            octant |= 1
+        if by >= y:
+            octant |= 2
+        if bz >= z:
+            octant |= 4
+
+        return None
+
 
 
 class ThreeDeeBeeTree(Generic[I]):
@@ -55,7 +67,19 @@ class ThreeDeeBeeTree(Generic[I]):
         return node.item
 
     def get_tree_node_by_key(self, key: Point) -> BeeNode:
-        raise NotImplementedError()
+        current = self.root
+        while current is not None:
+            if current.key == key:
+                return current
+            octant = 0
+            if key[0] >= current.key[0]:
+                octant |= 1
+            if key[1] >= current.key[1]:
+                octant |= 2
+            if key[2] >= current.key[2]:
+                octant |= 4
+            current = current.get_child_for_octant(octant)
+        raise KeyError(f"Key {key} not found in the tree.")
 
     def __setitem__(self, key: Point, item: I) -> None:
         self.root = self.insert_aux(self.root, key, item)
@@ -64,11 +88,30 @@ class ThreeDeeBeeTree(Generic[I]):
         """
             Attempts to insert an item into the tree, it uses the Key to insert it
         """
-        raise NotImplementedError()
+        if current is None:
+            return BeeNode(key, item)
+
+        octant = 0
+        if key[0] >= current.key[0]:
+            octant |= 1
+        if key[1] >= current.key[1]:
+            octant |= 2
+        if key[2] >= current.key[2]:
+            octant |= 4
+
+        child = current.get_child_for_octant(octant)
+        if child is None:
+            child = BeeNode(key, item)
+            current.set_child_for_octant(octant, child)
+        else:
+            current.set_child_for_octant(octant, self.insert_aux(child, key, item))
+
+        current.subtree_size += 1
+        return current
 
     def is_leaf(self, current: BeeNode) -> bool:
         """ Simple check whether or not the node is a leaf. """
-        raise NotImplementedError()
+        return all(child is None for child in current.children)
 
 if __name__ == "__main__":
     tdbt = ThreeDeeBeeTree()
